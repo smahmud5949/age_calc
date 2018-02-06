373f1943695be38cd860062c9dc46b25242356b2
smahmud5949 <35639245+smahmud5949@users.noreply.github.com>
2018-02-07 02:18:14 +0600

    Update AgeCalculator.py

--- AgeCalculator.py
+++ AgeCalculator.py
@@ -2,4 +2,4 @@ import datetime
 Running_year = datetime.datetime.now().year
 birth_year = int(input("Enter your birthday year: "))
 age = (Running_year - birth_year)
-print("Your are now %i years old" % (age))
+print("You are now %i years old" % (age))
